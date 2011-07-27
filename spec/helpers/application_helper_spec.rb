92e3887d7203f2b54753b90fa6e3ee68d3cc3106
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-07-26 22:28:56 -0400

    Simplify ApplicationHelper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,20 +2,18 @@ require 'spec_helper'
 
 describe ApplicationHelper do
   describe "#location" do
+    subject { helper.location }
+
     context "when no location is configured" do
-      it "returns nyc" do
-        helper.location.should == "NYC"
-      end
+      it { should == "NYC" }
     end
-    
+
     context "when a location is configured" do
       before do
         ENV["PIVOT_PONG_LOCATION"] = "Starbase Alpha"
       end
-      
-      it "returns that location" do
-        helper.location.should == "Starbase Alpha"
-      end
+
+      it { should == "Starbase Alpha" }
     end
   end
 end
