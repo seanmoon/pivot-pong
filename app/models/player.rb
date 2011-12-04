081d1f4c1b805a49576cf768bdcca54ad32adef6
Kris Hicks <krishicks@gmail.com>
2011-12-03 23:21:23 -0500

    Remove unused scopes from Player.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,4 @@
 class Player < ActiveRecord::Base
-  has_many :winning_matches, :class_name => 'Match', :foreign_key => 'winner_id'
-  has_many :losing_matches, :class_name => 'Match', :foreign_key => 'loser_id'
-
   before_validation :downcase_name
   before_save :clear_ranks_for_inactive_players
 
