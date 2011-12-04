36ecd3589641a471f1ba9f5fee41f7d0c9689c18
Kris Hicks <krishicks@gmail.com>
2011-12-03 23:14:23 -0500

    MatchPoint replaces MatchesHelper.
    With this change the notion of 30-day and 60-day rankings goes away in
    favor of a single all-time ranking that uses the official ratings
    process used by the National Association of Table Tennis. The new
    ranking is put next to the name of the person in the rankings for easy
    reference.
    
    This also removes a few defects associated with PivotPong, including
    rankings getting messed up on removal of old matches as rank isn't
    persisted.

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,8 +1,6 @@
 class MatchesController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: :create
 
-  include MatchesHelper
-
   def create
     winner, loser = params.values_at(:winner_name, :loser_name).map { |name|
       Player.find_or_create_by_name name.strip.downcase
@@ -28,11 +26,7 @@ class MatchesController < ApplicationController
   end
 
   def rankings
-    @rankings = Player.active.ranked
-    last_30_days_matches = Match.where("occured_at >= ?", 30.days.ago).order("occured_at asc")
-    last_90_days_matches = Match.where("occured_at >= ?", 90.days.ago).order("occured_at asc")
-    @last_30_days_rankings = calculate_rankings(last_30_days_matches)
-    @last_90_days_rankings = calculate_rankings(last_90_days_matches)
+    @rankings = MatchPoint.rankings
   end
 
   def players
