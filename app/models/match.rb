1adc776c01b49442da15c93645abc64495869b50
Kris Hicks <krishicks@gmail.com>
2011-12-04 00:22:06 -0500

    Clean up match.rb and match_spec.rb.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,16 +1,15 @@
 class Match < ActiveRecord::Base
   validates :winner,      presence: true
   validates :loser,       presence: true
-  validates :occured_at,  presence: true
 
   belongs_to :winner, :class_name => 'Player'
   belongs_to :loser, :class_name => 'Player'
 
-  before_validation :set_default_occured_at_date, on: :create
+  before_validation :set_default_occured_at_date, on: :create, :unless => ->{ occured_at.present? }
 
   private
 
   def set_default_occured_at_date
-    self.occured_at ||= Time.now
+    self.occured_at = Time.now
   end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,15 +1,34 @@
 require 'spec_helper'
 
 describe Match do
-  describe "setting default date" do
-    subject { Match.create }
-    let(:occured_at) { Time.parse("2011-03-27") }
-    before { Time.stub(:now).and_return(occured_at) }
-    its(:occured_at) { should == occured_at }
+  describe "associations" do
+    context "belongs_to" do
+      [:winner, :loser].each do |association|
+        it "should belong_to #{association}, class_name: 'Player'" do
+          reflection = Match.reflect_on_association(association)
+          reflection.macro.should == :belongs_to
+          reflection.class_name.should == "Player"
+        end
+      end
+    end
   end
 
   describe "validations" do
-    subject { Match.create }
-    it { should_not be_valid }
+    let(:match) { Match.new }
+
+    before { match.valid? }
+
+    it "should not be valid" do
+      match.errors.should include :winner, :loser
+    end
+  end
+
+  describe "callbacks" do
+    context "on create" do
+      subject { Match.create }
+      let(:occured_at) { Time.parse("2011-03-27") }
+      before { Time.stub(:now).and_return(occured_at) }
+      its(:occured_at) { should == occured_at }
+    end
   end
 end
